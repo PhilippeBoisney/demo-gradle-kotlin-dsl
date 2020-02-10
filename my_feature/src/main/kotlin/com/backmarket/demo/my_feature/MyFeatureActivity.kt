113dd28183d28daa6cadbae38400d20f4d65106c
Phil <phil.boisney@gmail.com>
2020-02-10 14:33:21 +0100

    feat(groovy): Add the Groovy based app

---
+++ my_feature/src/main/kotlin/com/backmarket/demo/my_feature/MyFeatureActivity.kt
@@ -0,0 +1,17 @@
+package com.backmarket.demo.my_feature
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+class MyFeatureActivity : AppCompatActivity(R.layout.activity_my_feature) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        log()
+    }
+
+    private fun log() {
+        Log.d(MyFeatureActivity::class.java.simpleName, "onCreate() is called")
+    }
+}
