e8dfdec4aa5ac9bb8584c55bc2575ccfacb55983
Phil <phil.boisney@gmail.com>
2020-02-10 18:21:50 +0100

    Add Glide sample

--- buildSrc/src/main/kotlin/Libraries.kt
+++ buildSrc/src/main/kotlin/Libraries.kt
@@ -9,4 +9,12 @@ object Libraries {
         const val appcompat = "androidx.appcompat:appcompat:1.2.0-alpha02"
         const val material = "com.google.android.material:material:1.1.0"
     }
+
+    object Glide {
+        private const val version = "4.10.0"
+
+        const val core = "com.github.bumptech.glide:glide:$version"
+        const val compiler = "com.github.bumptech.glide:compiler:$version"
+        const val okhttp = "com.github.bumptech.glide:okhttp3-integration:$version"
+    }
 }
